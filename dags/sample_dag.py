94061c2376a53d1e1dd09035f2e38e6780b0915c
smit-darji <smit.softvan@gmail.com>
2024-02-26 10:27:04 +0530

    feat: my first commit in dev

--- dags/sample_dag.py
+++ dags/sample_dag.py
@@ -1,8 +1,43 @@
+from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+import boto3
+import logging
 
-with DAG("sample_dag", schedule_interval="@daily") as dag:
-    start = DummyOperator(task_id="start")
-    end = DummyOperator(task_id="end")
 
-    start >> end
+def fetching_ssm_parameter():
+    session = boto3.session.Session()
+    client = session.client('ssm', region_name='us-east-1')
+    key_value = client.get_parameter(
+        Name='/testing/toplevel/ssmparameter', WithDecryption=True
+    ).get('Parameter').get('Value')
+    logging.info(key_value)
+    return key_value
+
+
+ssm_parameter_call = fetching_ssm_parameter()
+
+
+def print_fetching_ssm_parameter():
+    ssm_parameter_call = fetching_ssm_parameter()
+    logging.info(ssm_parameter_call)
+
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+}
+
+
+dag = DAG(
+    'example_top_level_code',
+    default_args=default_args,
+    description='DAG to test the top level code',
+    schedule_interval=None
+)
+
+ssm_parameter_task = PythonOperator(
+    task_id='top_level_code_task',
+    python_callable=print_fetching_ssm_parameter,
+    dag=dag,
+)

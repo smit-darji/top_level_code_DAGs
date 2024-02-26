c4c7b63c55e40dc731cf76976ec1d9be0f1b975e
smit-darji <smit.softvan@gmail.com>
2024-02-26 12:00:07 +0530

    feat: my first commit in dev

--- analyze_dags.py
+++ analyze_dags.py
@@ -13,9 +13,9 @@ def check_top_level_code(directory):
                         tree = ast.parse(file.read(), filename=file_path)
                         has_top_level_code = any(isinstance(node, (ast.FunctionDef, ast.ClassDef)) for node in tree.body)
                         if has_top_level_code:
-                            valid_files.append(filename)
-                        else:
                             invalid_files.append(filename)
+                        else:
+                            valid_files.append(filename)
                     except SyntaxError:
                         invalid_files.append(filename)
                         print(f"SyntaxError: Unable to parse {filename}")
@@ -27,10 +27,10 @@ directory = "dags"
 valid_files, invalid_files = check_top_level_code(directory)
 
 # Print valid and invalid filenames
-print("Valid files with top-level code:")
+print("Valid files with-out top-level code:")
 for file in valid_files:
     print(file)
 
-print("\nInvalid files:")
+print("\nInvalid files top-level code::")
 for file in invalid_files:
     print(file)

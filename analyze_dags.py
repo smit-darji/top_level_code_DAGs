ad91934134811b2ed0bdc7fd2065c930b2df5572
smit-darji <smit.softvan@gmail.com>
2024-02-26 12:17:47 +0530

    feat: my first commit in dev

--- analyze_dags.py
+++ analyze_dags.py
@@ -1,36 +1,37 @@
 import ast
-import os
+import sys
 
-def check_top_level_code(directory):
+def check_top_level_code(files):
     valid_files = []
     invalid_files = []
-    for root, dirs, files in os.walk(directory):
-        for filename in files:
-            if filename.endswith(".py"):
-                file_path = os.path.join(root, filename)
-                with open(file_path, "r") as file:
-                    try:
-                        tree = ast.parse(file.read(), filename=file_path)
-                        has_top_level_code = any(isinstance(node, (ast.FunctionDef, ast.ClassDef)) for node in tree.body)
-                        if has_top_level_code:
-                            invalid_files.append(filename)
-                        else:
-                            valid_files.append(filename)
-                    except SyntaxError:
-                        invalid_files.append(filename)
-                        print(f"SyntaxError: Unable to parse {filename}")
+    for file_path in files:
+        try:
+            with open(file_path, "r") as file:
+                tree = ast.parse(file.read(), filename=file_path)
+                has_top_level_code = any(isinstance(node, (ast.FunctionDef, ast.ClassDef)) for node in tree.body)
+                if has_top_level_code:
+                    invalid_files.append(file_path)
+                else:
+                    valid_files.append(file_path)
+        except SyntaxError:
+            invalid_files.append(file_path)
+            print(f"SyntaxError: Unable to parse {file_path}")
     return valid_files, invalid_files
 
-
 # Example usage:
-directory = "dags"
-valid_files, invalid_files = check_top_level_code(directory)
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python analyze_dags.py")
+        sys.exit(1)
 
-# Print valid and invalid filenames
-print("Valid files with-out top-level code:")
-for file in valid_files:
-    print(file)
+    files = sys.argv[1:]
+    valid_files, invalid_files = check_top_level_code(files)
 
-print("\nInvalid files top-level code::")
-for file in invalid_files:
-    print(file)
+    # Print valid and invalid filenames
+    print("Valid files without top-level code:")
+    for file in valid_files:
+        print(file)
+
+    print("\nInvalid files with top-level code:")
+    for file in invalid_files:
+        print(file)

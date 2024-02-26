7b5798725dfd3425cc848222e74f8593304fb1c5
smit-darji <smit.softvan@gmail.com>
2024-02-26 11:44:17 +0530

    feat: my first commit in dev

--- dags/dag2.py
+++ dags/dag2.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.operators.dummy_operator import DummyOperator, DummyOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 default_args = {
     'owner': 'airflow',
